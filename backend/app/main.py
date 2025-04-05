7f09de3087c10482a2e412729f8cbb6a6da170cf
RanaMuhammadUmair <ranaumairb@gmail.com>
2025-04-05 00:35:05 +0000

    worked on route summarize.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.routes import auth
 
+from app.routes import summarize
+
 
 app = FastAPI()
 # Create tables (optional, only for first-time setup)
 Base.metadata.create_all(bind=engine)
-
+app.include_router(summarize.router)
 app.include_router(auth.router)
 
 
